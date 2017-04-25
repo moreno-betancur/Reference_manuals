168abb4f6e8f40676ea23e2de2f7d0db28e63954
moreno-betancur <margarita.moreno@mcri.edu.au>
2017-04-25 11:05:11 +1000

    First commit

---
+++ create_ref_manual.R
@@ -0,0 +1,12 @@
+# for pdf
+pack <- "survMCOD"
+path <- find.package(pack)
+system(paste(shQuote(file.path(R.home("bin"), "R")),
+             "CMD", "Rd2pdf", shQuote(path)))
+
+
+# for pdf
+pack <- "survtd"
+path <- find.package(pack)
+system(paste(shQuote(file.path(R.home("bin"), "R")),
+             "CMD", "Rd2pdf", shQuote(path)))
